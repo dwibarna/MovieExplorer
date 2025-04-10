0de8b1a2194f3dccda09f2421a6fa825887e850f
dwiajisobarna <dwiajisobarna1@gmail.com>
2025-04-10 10:33:33 +0700

    Initial Commit

---
+++ MovieExplorer/MovieExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieExplorerApp.swift
+//  MovieExplorer
+//
+//  Created by Dwi Aji Sobarna on 10/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
